d4baf1536087e6b63c9faf9c30a880a66be9701e
Offliner <42777325+Offliners@users.noreply.github.com>
2019-03-05 16:40:05 +0800

    Update answer.py

--- Miscellaneous/what base is this/answer.py
+++ Miscellaneous/what base is this/answer.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python
 from pwn import * 
 
-s = remote('2018shell1.picoctf.com', 31711) 
+r = remote('2018shell1.picoctf.com', 31711) 
 
-binary = s.recvuntil('word.') 
+binary = r.recvuntil('word.') 
 binary = re.findall(r'(\d+)', binary) 
 ans = ''
 for i in binary:
 	ans += chr(int(i, 2)) 
-s.sendline(ans) 
+r.sendline(ans) 
 
-hexa = s.recvuntil('word').strip()
+hexa = r.recvuntil('word').strip()
 hexa = re.findall(r'([0-9a-f]+) as ', hexa)[0]
 ans = hexa.decode('hex')
-s.sendline(ans)
+r.sendline(ans)
 
-octal = s.recvuntil('word.')[2:]
+octal = r.recvuntil('word.')[2:]
 octal = re.findall(r'[0-9]+', octal)
 ans = ''
 for i in octal:
 	ans += chr(int(i, 8))
-s.sendline(ans)
+r.sendline(ans)
 
-print s.recvuntil('}\n') 
+print r.recvuntil('}\n') 
 
-s.close() 
+r.close() 
